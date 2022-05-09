19ebacb33ec56e34e1c3a3edf94d4951b25cdac0
ctatienanh <102956829+ctatienanh@users.noreply.github.com>
2022-05-09 12:08:36 +0700

    tap su dung bien

---
+++ src/Bien.java
@@ -0,0 +1,11 @@
+public class Bien {
+    public static void main(String[] args) {
+        int i = 20;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = " Ha noi";
+        System.out.println("s =" +s);
+    }
+}
